936e59f614c67323b3281953d73bf05464474f82
Tai Ha <taihavn1101@gmail.com>
2024-10-16 12:38:30 +1100

    fix error

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,7 @@
 'use client'
 
 import { useState } from 'react'
-import { Bus, Train, Bike, Car,Ship } from 'lucide-react'
+import { Bus, Train, Bike, Car,Ship, MessageSquare } from 'lucide-react'
 import { Button } from "@/components/ui/button"
 import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from "@/components/ui/card"
 import { Label } from "@/components/ui/label"
